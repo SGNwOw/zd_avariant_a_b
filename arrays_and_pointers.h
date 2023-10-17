ef50abd89654dcdf76a2e57eae1dc4ac5d04d328
SGNwOw <herasimchuk2808@gmail.com>
2023-10-17 21:34:05 +0300

    start 3.1.2

--- arrays_and_pointers.h
+++ arrays_and_pointers.h
@@ -207,71 +207,173 @@ double sum_between_maximum_and_minimum_modules(T arr,int size) {//11
 	return sum;
 }
  ////////////////////////////3.1.2
-  /*template<typename T>
- double output_indices_that_multiples_2_and_number_numbers_that_multiples_2(T arr, int size) {
-	 int c
-	 for (int i = 0; i < size; i++)
-	 {
-		 if ((*(arr+i))/pow(2,i)
-		 {
-
-		 }
-	 }
- }*/
- template<typename T>//1
- double output_numbers_that_are_powers_of_2(T arr,int size) {
+ template<typename T>//1,2,3
+ double output_numbers_that_are_powers_of_value(T arr,int size,int value) {
 	 int count = 0;
 	 for (int i = 0; i < size; i++) {
 		 int num = *(arr+i);
-		 int temp = num;
-		 while (temp % 2 == 0 && temp > 1) {
-			 temp /= 2;
+		 while (num % value == 0 && num > 1) {
+			 num /= value;
 		 }
-		 if (temp == 1) {
+		 if (num == 1) {
 			 count++;
 		 }
 	 }
-	 for (int i = 1; i < size; i*=2)
+	 for (int i = 1; i < size; i*=value)
 	 {
 		 std::cout << *(arr + i) << std::endl;
 	 }
 	 return count;
  }
- template<typename T>//2 
- double output_numbers_that_are_powers_of_3(T arr, int size) {
+ template<typename T>//4
+ double output_indices_and_numbers_of_equal_perfect_squares(T arr, int size) {
 	 int count = 0;
 	 for (int i = 0; i < size; i++) {
-		 int num = *(arr + i);
-		 int temp = num;
-		 while (temp % 3  == 0 && temp > 1) {
-			 temp /= 3;
-		 }
-		 if (temp == 1) {
+		 if (sqrt(i) == floor(sqrt(i))) {
 			 count++;
 		 }
 	 }
-	 for (int i = 1; i < size; i*=3)
+	 for (int i = 0; i*i < size;i++)
 	 {
-		 std::cout << *(arr + i) << std::endl;
+		 std::cout << *(arr + i*i) << std::endl;
 	 }
 	 return count;
  }
- template<typename T>//3
- double output_numbers_that_are_powers_of_5(T arr, int size) {
-	 int count = 0;
-	 for (int i = 0; i < size; i++) {
-		 int num = *(arr + i);
-		 int temp = num;
-		 while (temp % 5 == 0 && temp > 1) {
-			 temp /= 5;
-		 }
-		 if (temp == 1) {
-			 count++;
-		 }
-	 }
-	 for (int i = 1; i < size; i *= 5)
-	 {
-		 std::cout << *(arr + i) << std::endl;
-	 }
-	 return count;
- }
+template<typename T>//5
+double output_indices_of_prime_numbers_and_themselves(T arr, int size) {
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		int divider = 2;
+		while ((divider < *(arr+i) - 1) && (*(arr+i) % divider) != 0)
+		{
+			divider++;
+		}
+		if ((*(arr + i) % divider) != 0) {
+			std::cout << *(arr + i) << std::endl;
+			count++;
+		}
+	}
+	return count;
+}
+template<typename T>
+double index_output_fibonacci_numbers_and_themselves(T arr, int size) {//?
+	int count = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (*(arr+i+3)==*(arr+i+2)+*(arr+i+1))
+		{
+			std::cout << *(arr + i) << std::endl;
+			count++;
+		}
+	}
+	return count;
+}
+ /////////////////////////////////////////////////3.1.3
+template<typename T>
+double combining_and_finding_sum_of_array(T arr3, T arr1, T arr2,int size1,int size2,int size3) {//1
+	int sum = 0;
+	for (int i = 0; i < size2; i++)
+	{
+		*(arr3 + i) = *(arr2 + i);
+	}
+	for (int i = 0; i < size1; i++)
+	{
+		*(arr3 + size1 + i) = *(arr1 + i);
+	}
+	for (int i = 0;  i < size3;  i++)
+	{
+		if (*(arr3+i)>0)
+		{
+			sum += *(arr3 + i);
+		}
+	}
+return sum;
+}
+template<typename T>
+double combining_and_find_negative_maximum_of_array(T arr3, T arr1, T arr2, int size1, int size2, int size3) {//2
+	int sum = 0;
+	int max = -10;
+	for (int i = 0; i < size2; i++)
+	{
+		*(arr3 + i) = *(arr2 + i);
+	}
+	for (int i = 0; i < size1; i++)
+	{
+		*(arr3 + size1 + i) = *(arr1 + i);
+	}
+	for (int i = 0; i < size3; i++)
+	{
+		if (*(arr3+i)<0)
+		{
+			if (*(arr3 + i) > max)
+			{
+				max = *(arr3 + i);
+			}
+		}
+	}
+	return max;
+}
+template<typename T>
+double finding_maximum_sum_pairs_of_numbers(T arr, int size) {//3
+	int max_sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		if (*(arr) + *(arr + size - i) > max_sum)
+		{
+			max_sum = *(arr) + *(arr + size - i);
+		}
+	}
+	return max_sum;
+}
+template <typename T>
+double finding_minimum_sum_pairs_of_numbers(T arr, int size) {//4
+	int min_sum = 100;
+	for (int i = 0; i < size; i++)
+	{
+		if (*(arr+i) + *(arr + size-1-i) < min_sum)
+		{
+			min_sum = *(arr+i)+*(arr+size-1-i);
+		}
+	}
+	return min_sum;
+}
+template<typename T>
+double find_of_products_of_pairs(T arr, int size) {//5
+	int sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += *(arr + i) * *(arr + i + 1);
+	}
+	return sum;
+}
+template<typename T>
+void display_elements_that_occur_only_once(T arr, int size) {
+	/*for (int i = 0; i < size; ++i) {
+		int count = 0;
+		for (int j = 0; j < size; ++j) {
+			if (i != j && *(arr + i) == *(arr + j)) {
+				++count;
+			}
+		}
+		if (!count) {
+			std::cout << *(arr + i) << std::endl;
+		}
+	}
+}*/
+	for (int i = 0; i < size; i++)
+	{
+		int count = 0;
+		for (int j = 0; j < size; j++)
+		{
+			if (i!=j && *(arr + i) == *(arr + j))
+			{
+				++count;
+			}
+		}
+		if (!count)
+		{
+			std::cout << *(arr + i) << std::endl;
+		}
+	}
+}
--- zdvariantab.cpp
+++ zdvariantab.cpp
@@ -1,6 +1,13 @@
 ﻿#include "arrays_and_pointers.h"
+#include "loops.h"
 int main() {
 	const int size = 10;
-	int arr[size]{2,0,3,1,9,2,-10,-2,8,3};
-	std::cout << output_numbers_that_are_powers_of_5(arr, size);
+	int arr[size]{1,1,3,3,5,6,7,8,9,15};
+	int arr1[] = { 1, -2, 3, -4, 5 };
+	int arr2[] = { -6, 7, -8, 9 ,-10};
+	const int size1 = sizeof(arr1) / sizeof(arr1[0]);
+	const int size2 = sizeof(arr2) / sizeof(arr2[0]);
+	const int size3 = size1 + size2;
+	int arr3[size3];
+	display_elements_that_occur_only_once(arr, size);
 }
